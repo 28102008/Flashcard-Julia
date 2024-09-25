ce7a3e5c15fd5c762da4766e6b67531e327bae8a
28102008 <164515096+28102008@users.noreply.github.com>
2024-09-25 20:53:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ECONOMIA GLOBAL',
+    'Com base na economia mundial, qual é o país mais rico do mundo?',
+    'os Estados Unidos lideram o ranking com um PIB de US$ 28,7 trilhões'
+)
+
+criaCartao(
+    'PAÍSES',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa.'
+)
+
+criaCartao(
+    'TERRITORIO',
+    'Diga um país que é transcontinental?',
+    'Russia'
+)
